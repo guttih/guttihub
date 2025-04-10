5ab942cf3c595ee460777d3b15b886185598f290
Guðjon Hólm Sigurðsson <gudjonholm@gmail.com>
2025-04-10 00:59:33 +0000

    updating formats

--- src/types/StreamFormat.ts
+++ src/types/StreamFormat.ts
@@ -2,8 +2,8 @@
 export enum StreamFormat {
     MP4 = 'mp4',
     MKV = 'mkv',
-    // TS = 'ts',
-    // AVI = 'avi',
+    TS = 'ts',
+    AVI = 'avi',
     //M3U8 = 'm3u8',
     // FLV = 'flv',
     // MOV = 'mov',
@@ -15,10 +15,10 @@ export enum StreamFormat {
 export const supportedFormats: StreamFormat[] = [
     StreamFormat.MP4,
     StreamFormat.MKV,
+    // StreamFormat.AVI, // Ttried a few, but no playback
+    // StreamFormat.TS, // Ttried a few, but no playback
     // StreamFormat.M3U8 //No wideo with this format was found on servers
     // StreamFormat.WEBM, // No wideo with this format was found on servers
      // StreamFormat.FLV, //No wideo with this format was found on servers
     // StreamFormat.MOV, //No wideo with this format was found on servers
-    // StreamFormat.AVI, // Ttried a few, but no playback
-    // treamFormat.TS, // Ttried a few, but no playback
   ];
